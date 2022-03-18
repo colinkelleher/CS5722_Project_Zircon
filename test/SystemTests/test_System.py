7a0b2794b183d98c90826ac340176fc4adecda55
ThibLMT <21165823@studentmail.ul.ie>
2022-03-18 15:29:52 +0000

    add Protected Id test

--- test/SystemTests/test_System.py
+++ test/SystemTests/test_System.py
@@ -5,4 +5,9 @@ from system.System import System
 class TestSystem(TestCase):
     def test_System_Creation_With_Id(self):
         system = System(1)
-        self.assertEqual(system.system_id, 1, 'Id is not equal to supposed value')
+        self.assertEqual(system._system_id, 1, 'Id is not equal to supposed value')
+
+    def test_Protected_System_Id_Cannot_Be_Modified(self):
+        system = System(1)
+        system._system_id = 5
+        self.assertEqual(system._system_id, 1, 'Id should not have been modified')
