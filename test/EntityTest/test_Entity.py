eadf25743c98480e7f6d44140f944a483a93a39e
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-21 23:41:43 +0000

    Checking Github actions

--- test/EntityTest/test_Entity.py
+++ test/EntityTest/test_Entity.py
@@ -7,10 +7,11 @@ from entity.Entity import Entity
 
 class TestEntity(TestCase):
     def test_set(self):
-        player = Entity()
-        poscomp = PositionComponent()
-        player.set(poscomp)
+        entity = Entity()
+        poscomp = Component()
+        entity.set(poscomp)
+        res = entity.get()
 
-        self.assertTrue(PositionComponent in player.components)
-        self.assertTrue(player.has(PositionComponent))
-        self.assertTrue(player.has(Component))
+        # self.assertTrue(PositionComponent in player.components)
+        # self.assertTrue(player.has(PositionComponent))
+        self.assertTrue(poscomp in res)
