f5c8235357c77122b77fded41e5dffbb1a9f96b2
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-21 21:24:36 +0000

    Creating DisplayComponent Test

--- test/ComponentTests/test_DisplayComponent.py
+++ test/ComponentTests/test_DisplayComponent.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
+from component.Component import Component
+from component.DisplayComponent import DisplayComponent
 
 class TestDisplayComponent(TestCase):
     def test_set_color(self):
-        self.fail()
+        dispcomp = DisplayComponent("Test",(1,1,1))
+        dispcomp.set_color((2,2,2))
+        self.assertTrue(dispcomp.color == (2,2,2))
