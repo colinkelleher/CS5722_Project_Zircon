64b7d43a4af20cf58edde6902c7afa1ecfb5dc03
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-22 16:04:40 +0000

    Trying test

--- system/MapGenerationSystem.py
+++ system/MapGenerationSystem.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import Tuple, List, TYPE_CHECKING
 import random
 
-from comtypes.safearray import numpy
+import numpy
 
 if TYPE_CHECKING:
     from entity import Entity
