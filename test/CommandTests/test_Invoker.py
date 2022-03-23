d382fc3a7c2ba5c1951f5c743387d71bb116ba0b
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-23 18:11:35 +0000

    Invoker Tests

--- test/CommandTests/test_Invoker.py
+++ test/CommandTests/test_Invoker.py
@@ -1,12 +1,21 @@
 from unittest import TestCase
+from command.Invoker import Invoker
+from command.ConcreteCommand import ConcreteCommand
+from command.Receiver import Receiver
 
 
 class TestInvoker(TestCase):
     def test_set_start(self):
-        self.fail()
+        inv = Invoker()
+        rec = Receiver()
+        com = ConcreteCommand(rec,"test1","test2")
+        inv.setStart(com)
+        self.assertTrue(inv.onStart == com)
 
     def test_set_finish(self):
-        self.fail()
+        inv = Invoker()
+        rec = Receiver()
+        com = ConcreteCommand(rec,"test1","test2")
+        inv.setFinish(com)
+        self.assertTrue(inv.onFinish == com)
 
-    def test_complete_task(self):
-        self.fail()
