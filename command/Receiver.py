9dec51ecad08e2d962c0c8189a5bf4872df573ae
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-23 16:29:47 +0000

    Receiver

---
+++ command/Receiver.py
@@ -0,0 +1,12 @@
+'''
+Receiver Class
+
+- Business Logic - where actions can be carried out
+'''
+class Receiver:
+    def completeTask2(self, b:str) -> None:
+        print("Receiver working on",b)
+
+
+    def completeTask1(self, a:str) -> None:
+        print("Receiver working on",a)
