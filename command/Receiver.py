cbd8ab3095a7f3b48eca2eaa10fe751728a7b83c
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-23 17:08:46 +0000

    Receiver

--- command/Receiver.py
+++ command/Receiver.py
@@ -4,8 +4,8 @@ Receiver Class
 - Business Logic - where actions can be carried out
 '''
 class Receiver:
-    def completeTask2(self, b:str) -> None:
-        print("Receiver working on",b)
+    def completeTask2(self, b:str) -> tuple[str, str]:
+        return ("Receiver working on", b)
 
 
     def completeTask1(self, a:str) -> None:
