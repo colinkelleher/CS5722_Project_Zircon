f8147c2ca73c2289ff9d54631f9ef7e5ae5b3682
Benjamin <21157146@studentmail.ul.ie>
2022-03-28 18:35:24 +0100

    Factory Method moved

--- factory/FactoryMethod.py
+++ factory/FactoryMethod.py
@@ -23,25 +23,6 @@ class MapGeneratorSimple(MapGenerator):
         return GameMapSimple()
 
 
-class GameMap(ABC):
-    """
-    The GameMap goal is creating Floor entities,
-    following some pattern depending on what concrete GameMap creator is used
-    """
-    @abstractmethod
-    def create_walls(self) -> str:
-        pass
-
-
-class GameMapSimple(GameMap, ABC):
-    """
-    The GameMapSimple creates only Floor tiles to draw rectangle shapes
-    """
-    def create_walls(self) -> str:
-        # Add here the code to create the walls needed for rectangle rooms
-        return "Simple Map is used."
-
-
 def client_code(creator: MapGenerator) -> None:
 
     print(f"{creator.create_map()}")
