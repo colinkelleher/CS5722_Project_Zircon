b3e2757ad1d8c255d112f1a6102de808622533df
ThibLMT <21165823@studentmail.ul.ie>
2022-03-28 22:24:46 +0100

    Better Screen Size

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ import tcod
 from definitions import ROOT_DIR
 
 # Screen size, in tiles
-screen_width = 80
-screen_height = 90
+screen_width = 90
+screen_height = 70
 
 # Load the font, a 32 by 8 tile font with libtcod's old character layout.
 path_to_tilesheet = os.path.join(ROOT_DIR, 'resources\\dejavu10x10_gs_tc.png')
