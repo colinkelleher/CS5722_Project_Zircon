2f86bf9d798dcebe326d5cb56dd93a4c9f9c69dd
Benjamin <21157146@studentmail.ul.ie>
2022-03-29 16:40:09 +0100

    Fix .png path

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ screen_width = 90
 screen_height = 70
 
 # Load the font, a 32 by 8 tile font with libtcod's old character layout.
-path_to_tilesheet = os.path.join(ROOT_DIR, 'resources\\dejavu10x10_gs_tc.png')
+path_to_tilesheet = os.path.join(ROOT_DIR, 'resources/dejavu10x10_gs_tc.png')
 tileset = tcod.tileset.load_tilesheet(path_to_tilesheet, 32, 8, tcod.tileset.CHARMAP_TCOD)
 
 # Map size
