5a7b1c875ebf78f220368775832b63aae547b078
Colin Kelleher <21160562@studentmail.ul.ie>
2022-03-29 19:03:00 +0100

    Down Command

---
+++ command/downCommand.py
@@ -0,0 +1,11 @@
+from command.Command import Command
+
+
+class downCommand(Command):
+    def __init__(self, poscomp):
+        self.poscomp = poscomp
+
+    def execute(self):
+        x = self.poscomp.y
+        z = x+1
+        self.poscomp.set_pos_y(z)
